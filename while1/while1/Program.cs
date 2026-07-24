9e89f6b0919491d0dbb6ceef70fbef5bd039b920
Kaizouz1 <joaoleiteferraco@gmail.com>
2026-07-23 21:44:47 -0300

    Add files via upload

---
+++ while1/while1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numeroSecreto = 8;
+        Console.Write("Adivinhe o número secreto: ");
+        int palpite = int.Parse(Console.ReadLine());
+       
+       while (palpite != numeroSecreto)
+        {
+            Console.WriteLine("Tente novamente!");
+            Console.Write("Digite outro número: ");
+            palpite = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Parabens, Você acertou o número secreto!");
+    }
+}
---
+++ while2/while2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite uma nota entre 0.0 e 10.0:");
+double nota = double.Parse(Console.ReadLine());
+
+while (nota < 0 || nota > 10)
+{
+    Console.WriteLine("Nota inválida! Digite uma nota entre 0.0 e 10.0:");
+    nota = double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Nota valida!");
